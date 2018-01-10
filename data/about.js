74307c25f7a90dd5c09b862a01faba66c9f48bbb
brandok11 <brandon.kielbasa@gmail.com>
2018-01-09 21:14:29 -0500

    Update about.js

--- data/about.js
+++ data/about.js
@@ -3,7 +3,6 @@ var aboutData = {
 		'Brandon Kielbasa is a Tenant Organizer and Housing Advocate with a broad range of experience in community organizing and one-on-one casework. He works for the Cooper Square Committee in New York City’s Lower East Side as the Director of Organizing and Policy.',
 		'He sees himself as a Social Change Practitioner and practices community organizing because he believes that it is essential for individuals and communities to be engaged in solving their own problems and for them to gain strength and knowledge by doing so.',
 		'For additional information on Brandon visit his <a href="http://www.linkedin.com/in/brandonkielbasa" title="Brandon on Linkedin" target="_blank">Linkedin</a> or <a href="http://www.idealist.org/view/user/bCTjCNzZzj74/" title="Brandon on Idealist" target="_blank">Idealist</a> profiles.',
-		'Contact him at: <a href="mailto:brandon.kielbasa@gmail.com" title="Send Brandon an email">brandon.kielbasa@gmail.com</a>',
-		'<a class="highlights" href="#real-time-enforcement">deep link</a>'
+		'Contact him at: <a href="mailto:brandon.kielbasa@gmail.com" title="Send Brandon an email">brandon.kielbasa@gmail.com</a>'
 	]
 };
